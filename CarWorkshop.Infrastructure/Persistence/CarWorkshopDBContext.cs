687fd1ce282b75cde0109c12037697b8264d213a
Szemin <majkeloos2137@gmail.com>
2023-07-21 11:40:00 +0200

    Third commit - Creating one table with properties, installing EF to project, next doing first migration

---
+++ CarWorkshop.Infrastructure/Persistence/CarWorkshopDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkshop.Infrastructure.Persistence
+{
+    public class CarWorkshopDBContext: DbContext  
+    {
+        public DbSet<CarWorkshop.Domain.Entities.CarWorkshop> CarWorkshops { get; set; }  
+        //DbSet prop representation table in database 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        //Overriding "OnConfiguring" method with connection string
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CarWorkshopDb;Trusted_Connection=True;"); 
+            //connection string to local database
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.Entities.CarWorkshop>().OwnsOne(c => c.ContactDetails); 
+            //ContactDetails is no longer another table,
+            //EF know now that ContactDetails is property in table and its representation of CarWorkshop table
+        }
+    }
+}
